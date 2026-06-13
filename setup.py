d0481aa7efc530e7a11275d937b2ccda79b1a86a
SYED SADATH G <syedsadath.ds8558@gmail.com>
2026-06-13 10:08:18 +0000

    author update

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ print(get_requirements())
 setup(
     name="AI-TRAVEL-PLANNER",
     version="0.0.1",
-    author="sunny savita",
-    author_email="snshrivas3365@gmail.com",
+    author="syed sadath g",
+    author_email="syedsadath.ds8558@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements()
 )
